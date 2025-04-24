aaeafa595c8899e17e337cb7f642545609341c67
Yaarub Nassr <yaarub.nasser@yahoo.com>
2025-04-24 15:45:58 +0200

    add create controller

---
+++ Profile/Controllers/ProfilesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Profile.Data;
+using Profile.Models;
+
+namespace Profile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfilesController(DataContext context) : ControllerBase
+    {
+        private readonly DataContext _context = context;
+
+        [HttpPost("create")]
+
+        public async Task <IActionResult> Create(ProfileRegistrationFrom form)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var profileEntity = new UserProfileEntity
+            {
+                UserId = form.UserId,
+                FirstName = form.FirstName,
+                LastName = form.LastName,
+                PhoneNumber = form.PhoneNumber
+            };
+
+
+            _context.Add(profileEntity);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+
+        }
+
+      
+    }
+}
---
+++ Profile/Models/ProfileRegistrationFrom.cs
@@ -0,0 +1,10 @@
+﻿namespace Profile.Models
+{
+    public class ProfileRegistrationFrom
+    {
+        public string UserId { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
